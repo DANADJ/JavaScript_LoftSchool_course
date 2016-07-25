d35ca4771cd5c9fce177e924902834c19ac35373
Valentin Gordienko <danadj@yandex.ru>
2016-07-25 15:15:08 +0300

    Добавил код в файл timer.js

--- DZ_6/DZ_6.1--Timer/timer.js
+++ DZ_6/DZ_6.1--Timer/timer.js
@@ -1,3 +1,23 @@
 /**
- * Created by DANADJ on 25.07.16.
+ Задача:
+ Создать модуль, который экспортирует функцию 'timer'.
+ Функция 'timer' должна возвращать новый промис.
+ Функция 'timer' принимает 1 аргумент - количество миллисекунд, через которые промис должен перейти в состояние 'fulfilled'.
+
+ Пример использования:
+ timer(3000).then(() => console.log('я вывелась через 3 секунды'))
  */
+
+function timer(ms){
+	return new Promise(function(resolve, reject){
+		var time = ms/1000;
+		setTimeout(()=>{
+			resolve(time);
+		}, ms);
+	});
+}
+
+//Проверка работы функции
+//timer(5000).then((time)=>console.log('Я отработала через '+time+' секунд.'));
+
+module.exports = timer;
